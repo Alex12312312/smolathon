f018bf44e87b18438415aa32f8d9ef1e6392b238
Alex <sasha_novosel@mail.ru>
2024-10-10 21:10:44 +0300

    ToTaskButton

--- frontend/src/pages/mainPage/mainPage.tsx
+++ frontend/src/pages/mainPage/mainPage.tsx
@@ -1,10 +1,10 @@
+import { Button } from '@/components/ui/button'
 import { CategoryCard } from '../../components/ui/categoryCard/categoryCard'
 import { Group } from '../../components/ui/group/group'
 
 export const MainPage = () => {
     return (
-        <>
-            <div className="h-full w-full">
+            <div className="h-full w-full flex flex-col justify-between">
                 <Group name="Категории" className="m-4">
                     <div className="flex w-full flex-1 flex-grow flex-wrap items-stretch gap-2 self-stretch">
                         <CategoryCard
@@ -28,7 +28,7 @@ export const MainPage = () => {
                         ></CategoryCard>
                     </div>
                 </Group>
+                <Button className="m-4 bg-white border text-green-500 border-green-500">Перейти к заданиям</Button>
             </div>
-        </>
     )
 }
