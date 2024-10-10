75adf3536b3606fb1cdcfc9140fd73672c4ff6ab
Alex <sasha_novosel@mail.ru>
2024-10-10 19:39:21 +0300

    newFixes2

--- frontend/src/pages/greetings/greetings.tsx
+++ frontend/src/pages/greetings/greetings.tsx
@@ -1,29 +1,12 @@
 import { useState } from "react"
 import smolensk from "../../assets/smolensk.png"
 import smolensk2 from "../../assets/smolensk2.png"
-import { Button } from "../../components/ui/button"
 import { useNavigate } from "react-router-dom"
 import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group"
 
-const GreetingsTexts = {
-    'page-one': 'Открой новые горизонты, почувствуй свой город!',
-    'page-two': 'Сохрани культурное наследие',
-    'page-three': 'У меня украли дизайнера',
-}
-
-const GreetingsImage = {
-    'page-one': smolensk,
-    'page-two': smolensk2,
-    'page-three': 'https://i.pinimg.com/736x/1f/84/15/1f84150a1bc0057df83347cfc05c272f.jpg',
-}
-
-const FirstScreen = () => {
-
-}
 
 export const Greetings = () => {
     const navigator = useNavigate();
-    const [selected, selectPage] = useState('first');
     return <div className="w-lvw h-lvh flex">
         <img className="w-full h-full object-cover" src={smolensk} alt="Изображение не загружено"></img>
         <button className="absolute top-0 bg-transparent right-0 p-3 size-auto text-white text-4xl " onClick={() => { navigator("/main") }}>⮾</button>
