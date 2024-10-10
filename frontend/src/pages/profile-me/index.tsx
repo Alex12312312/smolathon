dc34f007b546ffc7c6eec2f6c951642363ad6fc9
rh0danthe <rita.pichushkina.04@mail.ru>
2024-10-10 23:02:13 +0300

    fix api

--- frontend/src/pages/profile-me/index.tsx
+++ frontend/src/pages/profile-me/index.tsx
@@ -1,10 +1,11 @@
+import { useTelegram } from '@/lib/telegram/telegramProvider.tsx'
 import { useGetMe } from '../../hooks/user.hooks.getme.ts'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 function ProfileMe() {
-    const { user, error, isLoading } = useGetMe(
-        'query_id=AAHNCQ4xAAAAAM0JDjGb8oR-&user=%7B%22id%22%3A823003597%2C%22first_name%22%3A%22%D1%80%D0%B8%D1%82%D0%B0%20%E0%B6%9E%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22rh0danthe%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1728587662&hash=12934a89a58005feaa9e1404d444883b7ea78c722e70f2b13c2a633065febf01',
-    )
+    const telegram = useTelegram()
+
+    const { user, error, isLoading } = useGetMe(telegram.webApp?.initData ?? '')
 
     if (isLoading) {
         return <div>Loading...</div>
