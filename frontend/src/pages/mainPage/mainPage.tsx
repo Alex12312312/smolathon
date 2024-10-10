4d24d719152e389f3db6220b6cca9f8924fd45e4
quassbottle <quassbottleyt@gmail.com>
2024-10-10 19:48:43 +0300

    fix cards

--- frontend/src/pages/mainPage/mainPage.tsx
+++ frontend/src/pages/mainPage/mainPage.tsx
@@ -5,11 +5,28 @@ export const MainPage = () => {
     return (
         <>
             <Group name="Категории">
-                <CategoryCard
-                    className="bg-red-600"
-                    text="Text"
-                    imageSrc="https://avatars.mds.yandex.net/i?id=46500a898df3ace1c372768ac44af342_l-8177627-images-thumbs&n=13"
-                ></CategoryCard>
+                <div className="flex w-full flex-1 flex-grow flex-wrap items-stretch gap-2 self-stretch">
+                    <CategoryCard
+                        className="w-[200px] flex-grow bg-red-600"
+                        text="Text"
+                        imageSrc="https://avatars.mds.yandex.net/i?id=46500a898df3ace1c372768ac44af342_l-8177627-images-thumbs&n=13"
+                    ></CategoryCard>
+                    <CategoryCard
+                        className="flex-grow bg-red-600"
+                        text="Text"
+                        imageSrc="https://avatars.mds.yandex.net/i?id=46500a898df3ace1c372768ac44af342_l-8177627-images-thumbs&n=13"
+                    ></CategoryCard>
+                    <CategoryCard
+                        className="flex-grow bg-red-600"
+                        text="Text2"
+                        imageSrc="https://avatars.mds.yandex.net/i?id=46500a898df3ace1c372768ac44af342_l-8177627-images-thumbs&n=13"
+                    ></CategoryCard>
+                    <CategoryCard
+                        className="flex-grow"
+                        text="Text3"
+                        imageSrc="https://avatars.mds.yandex.net/i?id=46500a898df3ace1c372768ac44af342_l-8177627-images-thumbs&n=13"
+                    ></CategoryCard>
+                </div>
             </Group>
         </>
     )
