3a9c780c8dda4ffe1aea9370f5733f95268d9f4b
Alex <sasha_novosel@mail.ru>
2024-10-10 20:18:13 +0300

    radioButy

--- frontend/src/pages/greetings/greetings.tsx
+++ frontend/src/pages/greetings/greetings.tsx
@@ -2,7 +2,6 @@
 import smolensk from "../../assets/smolensk.png"
 //import smolensk2 from "../../assets/smolensk2.png"
 import { useNavigate } from "react-router-dom"
-import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group"
 
 
 export const Greetings = () => {
@@ -10,8 +9,8 @@ export const Greetings = () => {
     return <div className="w-lvw h-lvh flex">
         <img className="w-full h-full object-cover" src={smolensk} alt="Изображение не загружено"></img>
         <button className="absolute top-0 bg-transparent right-0 p-3 size-auto text-white text-4xl " onClick={() => { navigator("/main") }}>⮾</button>
-        <div className="absolute flex flex-col bg-[#302B2BCF] bottom-0 h-1/3 w-full rounded-lg pt-16 gap-[60px]">
-            <div className="w-64 text-center">Test Text</div>
+        <div className="absolute flex flex-col bg-[#302B2BCF] text-white bottom-0 h-1/3 w-full rounded-lg pt-16 gap-[60px] justify-center content-center">
+            <div className="w-4/5 text-center self-center text-2xl">Открой новые горизонты, почувствуй  свой город !</div>
             <RadioGroup className="flex flex-row" defaultValue="page-one">
                 <div className="flex items-center space-x-2">
                     <RadioGroupItem className="h-4 w-4" value="page-one" id="page-one" />
