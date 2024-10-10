7de8c71b3c1b665c38511f3a4e171bd2aa744fd9
Alex <sasha_novosel@mail.ru>
2024-10-10 20:34:17 +0300

    RadioGroupFixPosition

--- frontend/src/pages/greetings/greetings.tsx
+++ frontend/src/pages/greetings/greetings.tsx
@@ -12,7 +12,7 @@ export const Greetings = () => {
         <button className="absolute top-0 bg-transparent right-0 p-3 size-auto text-white text-4xl " onClick={() => { navigator("/main") }}>⮾</button>
         <div className="absolute flex flex-col bg-[#302B2BCF] text-white bottom-0 h-1/3 w-full rounded-lg pt-16 gap-[60px] justify-center content-center">
             <div className="w-4/5 text-center self-center text-2xl">Открой новые горизонты, почувствуй  свой город !</div>
-            <RadioGroup className="flex flex-row gap-[44px]" defaultValue="page-one">
+            <RadioGroup className="flex flex-row gap-[44px] self-center" defaultValue="page-one">
                 <div className="flex items-center space-x-2">
                     <RadioGroupItem className="h-4 w-4" value="page-one" id="page-one" />
                 </div>
