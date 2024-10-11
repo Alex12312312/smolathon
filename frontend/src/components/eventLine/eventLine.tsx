52037f3bfb7b2bd5b82a0faeb488bf0f75ac1eea
Alex <sasha_novosel@mail.ru>
2024-10-11 10:35:49 +0300

    lastcom

--- frontend/src/components/eventLine/eventLine.tsx
+++ frontend/src/components/eventLine/eventLine.tsx
@@ -9,7 +9,7 @@ export const EventLine = (props: IEventLine) => {
         <div className="flex h-12 w-11/12 flex-row self-center justify-between">
             <img className="object-scale size-12 rounded-full mr-3" src={props.picture}></img>
             <div className="flex w-3/4 flex-col">
-                <div className="font-bold text-black">{props.title}</div>
+                <div className="font-bold">{props.title}</div>
                 <div className="font-bold text-[#707070]">{props.type}</div>
             </div>
             <button className="w-fit rounded bg-green-500 px-3 py-1 text-white">Билеты</button>
--- frontend/src/pages/poster/index.tsx
+++ frontend/src/pages/poster/index.tsx
@@ -4,11 +4,6 @@ import Letter from '../../assets/letter.svg'
 export const Poster = () => {
     return <div className="w-full h-full flex flex-col gap-3">
         <div className="w-full flex flex-row justify-between content-center gap-1">
-            <div className="select-none text-xl p-4 font-bold">Афиша</div>
-            <div className='rounded flex w-[40%] p-4 border gap-1 mt-2 mr-2'>
-            <img className='select-none rounded-full' src={Letter}></img>
-            <div className='select-none'> Pick a date </div>
-            </div>
         </div>
         <div className='flex flex-col gap-4'>
         <EventLine picture={smolensk} title='Смоленская башня' type='Спектакль'></EventLine>
