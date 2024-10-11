4d88e9c33ca15565ee5d75da54a1df900d7bef2b
Re2n <111135217+Re2n@users.noreply.github.com>
2024-10-11 04:34:49 +0300

    add filter

--- backend/apps/api/src/asset/asset.controller.ts
+++ backend/apps/api/src/asset/asset.controller.ts
@@ -18,7 +18,7 @@ import { TelegramAuthGuard } from '@app/common/auth/telegram/auth-telegram.guard
 import { AssetCreateDto } from './dto/asset-create.dto'
 import { TelegramContextInterceptor } from '../auth/interceptors/telegram-context.interceptor'
 import { RequestWithTelegramContext } from '@app/common/controller/controller.model'
-import { ReqWithTelegramContextAndUser } from '../auth/auth.types'
+import { Category } from '@prisma/client'
 
 @ApiTags('Asset')
 @ApiSecurity('telegram-query')
@@ -35,12 +35,14 @@ export class AssetController {
     name: 'offset',
     required: false,
   })
+  @ApiQuery({ name: 'category', enum: Category, required: false })
   @Get()
   async findAll(
     @Query('limit', new ParseIntPipe({ optional: true })) limit?: null,
     @Query('offset', new ParseIntPipe({ optional: true })) offset?: null,
+    @Query('category') category?: Category,
   ) {
-    return mapToArrayResponse(await this.assetService.findAll({ limit, offset }), offset)
+    return mapToArrayResponse(await this.assetService.findAll({ limit, offset, category }), offset)
   }
 
   @ApiOkResponse({ type: AssetModel })
@@ -48,8 +50,8 @@ export class AssetController {
   @UseGuards(TelegramAuthGuard)
   @UseInterceptors(TelegramContextInterceptor)
   @Post()
-  async createAsset(@Body() dto: AssetCreateDto, @Req() req: ReqWithTelegramContextAndUser) {
-    return await this.assetService.create({ data: { ...dto, creatorId: req.user.id } })
+  async createAsset(@Body() dto: AssetCreateDto, @Req() req: RequestWithTelegramContext) {
+    return await this.assetService.create({ data: { ...dto, creatorId: BigInt(req.context.id) } })
   }
 
   @ApiOkResponse({ type: AssetModel })
--- backend/apps/api/src/telegram-bot/telegram-bot.update.ts
+++ backend/apps/api/src/telegram-bot/telegram-bot.update.ts
@@ -1,9 +1,10 @@
-// import { Update, Start, TelegrafContext, Ctx } from 'nestjs-telegraf'
+// import { Update, Start, Ctx } from 'nestjs-telegraf'
+// import { Context } from 'telegraf'
 
 // @Update()
 // export class TelegramBotUpdate {
 //   @Start()
-//   async start(@Ctx() ctx: TelegrafContextType) {
-//     await ctx.
+//   async start(@Ctx() ctx: Context) {
+//     await ctx.sendMessage('welcome')
 //   }
 // }
