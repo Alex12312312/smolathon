9eafdec7fd4671c4999303dd5aa526218ad2e857
rh0danthe <rita.pichushkina.04@mail.ru>
2024-10-11 09:27:28 +0300

    asfasf

--- frontend/src/components/task/task-item.tsx
+++ frontend/src/components/task/task-item.tsx
@@ -1,36 +1,39 @@
 import coin from '@/assets/coin.svg'
 import { Button } from '../ui/button'
-import { useState } from 'react';
-import TaskPopup from './task-popup';
+import { useState } from 'react'
+import TaskPopup from './task-popup'
 function TaskItem() {
-    const [isPopupOpen, setPopupOpen] = useState(false);
+    const [isPopupOpen, setPopupOpen] = useState(false)
 
     const handleOpenPopup = () => {
-        setPopupOpen(true);
-    };
+        setPopupOpen(true)
+    }
 
     const handleClosePopup = () => {
-        setPopupOpen(false);
-    };
+        setPopupOpen(false)
+    }
 
     return (
         <div>
             <div className="flex flex-row justify-between gap-4">
-                <div className='flex flex-col justify-between gap-4'>
-                    <div className='text-base'>«Смоленск через призму стекла»</div>
-                    <div className='flex flex-row gap-2 items-center'>
-                        <div className='text-[#FAA629] text-sm font-medium'> +1000</div>
+                <div className="flex flex-col justify-between gap-4">
+                    <div className="text-base">«Смоленск через призму стекла»</div>
+                    <div className="flex flex-row items-center gap-2">
+                        <div className="text-sm font-medium text-[#FAA629]"> +1000</div>
                         <img src={coin} />
                     </div>
                 </div>
-                <Button onClick={handleOpenPopup} className='text-sm text-gray-500 border-2 border-gray-500 self-center bg-transparent'>
+                <Button
+                    onClick={handleOpenPopup}
+                    className="self-center border-2 border-gray-500 bg-transparent text-sm text-gray-500"
+                >
                     Выполнить
                 </Button>
             </div>
 
             {isPopupOpen && <TaskPopup onClose={handleClosePopup} />}
         </div>
-    );
+    )
 }
 
 export default TaskItem
