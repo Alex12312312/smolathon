05cf69f8b808b0d278e8d9c4849aa1cd1bf878a6
rh0danthe <rita.pichushkina.04@mail.ru>
2024-10-11 09:53:29 +0300

    asas

--- frontend/src/components/task/task-list.tsx
+++ frontend/src/components/task/task-list.tsx
@@ -1,18 +1,19 @@
 import { Accordion, AccordionItem } from '@/components/ui/accordion'
 import TaskItem from './task-item'
 
+
 function TaskList() {
     return (
-        <div className='px-7 overflow-y-scroll'>
+        <div className='px-7 overflow-y-scroll w-full'>
             <Accordion type="single" collapsible={false} className="divide-y divide-gray-200 overflow-y-scroll">
                 <AccordionItem value="item-1" className="py-2">
-                    <TaskItem/>
+                    <TaskItem title="Смоленск через призму стекла"/>
                 </AccordionItem>
                 <AccordionItem value="item-2" className="py-2">
-                <TaskItem/>
+                <TaskItem title="Изучи смоленские достопримечательности"/>
                 </AccordionItem>
                 <AccordionItem value="item-3" className="py-2">
-                <TaskItem/>
+                <TaskItem title="Смоленск в картинах"/>
                 </AccordionItem>
             </Accordion>
         </div>
